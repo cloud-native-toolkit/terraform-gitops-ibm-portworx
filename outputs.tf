fbcffbd4e506ce044eb291cf17486fe4b23322ed
Sean Sundberg <seansund@us.ibm.com>
2022-09-05 22:28:31 -0500

    Changes to use gitops provider (#24)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -2,47 +2,47 @@
 output "name" {
   description = "The name of the module"
   value       = local.name
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "branch" {
   description = "The branch where the module config has been placed"
   value       = local.application_branch
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "namespace" {
   description = "The namespace where the module will be deployed"
   value       = local.namespace
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "server_name" {
   description = "The server where the module will be deployed"
   value       = var.server_name
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "layer" {
   description = "The layer where the module is deployed"
   value       = local.layer
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "type" {
   description = "The type of module where the module is deployed"
   value       = local.type
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "region" {
   description = "The region where the portworx resource has been delivered"
   value       = var.region
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "resource_group_id" {
   description = "The id of the resource group where the portworx resource has been delivered"
   value       = var.resource_group_id
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
