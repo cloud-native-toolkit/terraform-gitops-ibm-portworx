efd6dd6e78f2bb7d505ae7ea9001dc97ade8c9b9
Sean Sundberg <seansund@us.ibm.com>
2023-02-01 08:06:04 -0600

    Updates output values
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -48,17 +48,17 @@ output "resource_group_id" {
 }
 
 output "rwx_storage_class" {
-  value       = ""
+  value       = "portworx-rwx-gp3-sc"
   depends_on  = [gitops_module.module]
 }
 
 output "rwo_storage_class" {
-  value = "ibmc-vpc-block-10iops-tier"
+  value       = "portworx-gp3-sc"
   depends_on  = [gitops_module.module]
 }
 
 output "file_storage_class" {
-  value       = ""
+  value       = "portworx-gp3-sc"
   depends_on  = [gitops_module.module]
 }
 
@@ -66,3 +66,42 @@ output "block_storage_class" {
   value       = "ibmc-vpc-block-10iops-tier"
   depends_on  = [gitops_module.module]
 }
+
+output "storage_classes_provided" {
+  value      = [
+    "portworx-cassandra-sc",
+    "portworx-couchdb-sc",
+    "portworx-db-gp",
+    "portworx-db-gp2-sc",
+    "portworx-db-gp3-sc",
+    "portworx-db2-fci-sc",
+    "portworx-db2-rwo-sc",
+    "portworx-db2-rwx-sc",
+    "portworx-dv-shared-gp",
+    "portworx-dv-shared-gp3",
+    "portworx-elastic-sc",
+    "portworx-gp3-sc",
+    "portworx-kafka-sc",
+    "portworx-metastoredb-sc",
+    "portworx-nonshared-gp2",
+    "portworx-rwx-gp-sc",
+    "portworx-rwx-gp2-sc",
+    "portworx-rwx-gp3-sc",
+    "portworx-shared-gp",
+    "portworx-shared-gp-allow",
+    "portworx-shared-gp1",
+    "portworx-shared-gp3",
+    "portworx-solr-sc",
+    "portworx-watson-assistant-sc",
+    "px-db",
+    "px-db-cloud-snapshot",
+    "px-db-cloud-snapshot-encrypted",
+    "px-db-encrypted",
+    "px-db-local-snapshot",
+    "px-db-local-snapshot-encrypted",
+    "px-replicated",
+    "px-replicated-encrypted"
+  ]
+  depends_on  = [gitops_module.module]
+}
+
